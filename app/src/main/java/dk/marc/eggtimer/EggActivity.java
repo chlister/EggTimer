9054b90796fd6fd6107ccb19dfd5aa56f52dab1f
Marc Rohwedder Kær <zbcmakar16@zbc.dk>
2019-03-01 15:34:30 +0100

    Errors in the saved state - needs fixing

--- app/src/main/java/dk/marc/eggtimer/EggActivity.java
+++ app/src/main/java/dk/marc/eggtimer/EggActivity.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 import java.util.Locale;
 
 public class EggActivity extends AppCompatActivity {
-    private static final long START_TIME_SOFT_BOILED_IN_MILLIS = 300000;
+    private static final long START_TIME_SOFT_BOILED_IN_MILLIS = 3000;
     private static final long START_TIME_MEDIUM_BOILED_IN_MILLIS = 420000;
     private static final long START_TIME_HARD_BOILED_IN_MILLIS = 600000;
 
@@ -30,34 +30,61 @@ public class EggActivity extends AppCompatActivity {
         mTextViewCountDown = findViewById(R.id.timer_value);
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putLong("millisLeft", mTimeLeftInMillis);
+        outState.putBoolean("timerRunning", mTimerRunning);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        // Get the values needed from saved state
+        mTimerRunning = savedInstanceState.getBoolean("timerRunning");
+        mTimeLeftInMillis = savedInstanceState.getLong("millisLeft");
+        // We need to set the text again
+        updateCountDownText();
+
+        // if the timer is running we need to start it again from where it left off
+        if (mTimerRunning) {
+            startTimer(mTimeLeftInMillis);
+            mButtonStartStop.setEnabled(true);
+        }
+    }
+
     /**
      * Sets the timer text and enables the button for starting the timer.
      *
      * @param view
      */
     public void onButtonEggSelectedClicked(View view) {
-        switch (view.getId()) {
-            case R.id.button_soft:
-                // Enable start_button
-                mButtonStartStop.setEnabled(true);
-                // Set timer to 5:00
-                mTextViewCountDown.setText("05:00");
-                mCurrentTimeSelected = START_TIME_SOFT_BOILED_IN_MILLIS;
-                break;
-            case R.id.button_medium:
-                mButtonStartStop.setEnabled(true);
-                // set timer to 7:00
-                mTextViewCountDown.setText("07:00");
-                mCurrentTimeSelected = START_TIME_MEDIUM_BOILED_IN_MILLIS;
-                break;
-            case R.id.button_hard:
-                mButtonStartStop.setEnabled(true);
-                // set timer to 10:00
-                mTextViewCountDown.setText("10:00");
-                mCurrentTimeSelected = START_TIME_HARD_BOILED_IN_MILLIS;
-                break;
-            default:
-                throw new RuntimeException("Unknown button id");
+        if (!mTimerRunning) {
+            switch (view.getId()) {
+                case R.id.button_soft:
+                    // Enable start_button
+                    mButtonStartStop.setEnabled(true);
+                    // Set timer to 5:00
+                    mTextViewCountDown.setText(getCurrentSelectedMillisAsString(START_TIME_SOFT_BOILED_IN_MILLIS));
+                    mCurrentTimeSelected = START_TIME_SOFT_BOILED_IN_MILLIS;
+                    break;
+                case R.id.button_medium:
+                    mButtonStartStop.setEnabled(true);
+                    // set timer to 7:00
+                    mTextViewCountDown.setText(getCurrentSelectedMillisAsString(START_TIME_MEDIUM_BOILED_IN_MILLIS));
+                    mCurrentTimeSelected = START_TIME_MEDIUM_BOILED_IN_MILLIS;
+                    break;
+                case R.id.button_hard:
+                    mButtonStartStop.setEnabled(true);
+                    // set timer to 10:00
+                    mTextViewCountDown.setText(getCurrentSelectedMillisAsString(START_TIME_HARD_BOILED_IN_MILLIS));
+                    mCurrentTimeSelected = START_TIME_HARD_BOILED_IN_MILLIS;
+                    break;
+                default:
+                    throw new RuntimeException("Unknown button id");
+            }
         }
     }
 
@@ -71,6 +98,12 @@ public class EggActivity extends AppCompatActivity {
         String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
 
         mTextViewCountDown.setText(timeLeftFormatted);
+    }
+
+    private String getCurrentSelectedMillisAsString(long selected) {
+        int minutes = (int) (selected / 1000) / 60;
+        int seconds = (int) (selected / 1000) % 60;
+        return String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
     }
 
     /**
@@ -101,9 +134,12 @@ public class EggActivity extends AppCompatActivity {
 
             @Override
             public void onFinish() {
-
+                mTimerRunning = !mTimerRunning;
+                mButtonStartStop.setText(R.string.eggtivity_start);
+                mButtonStartStop.setEnabled(false);
+                mTextViewCountDown.setText("00:00");
             }
-        }.start();
+        }.start(); // Because we want to start this timer right now
         mTimerRunning = !mTimerRunning;
         mButtonStartStop.setText(R.string.eggtivity_stop);
     }
@@ -117,4 +153,5 @@ public class EggActivity extends AppCompatActivity {
         mTimerRunning = !mTimerRunning;
         mButtonStartStop.setEnabled(false);
     }
+
 }
