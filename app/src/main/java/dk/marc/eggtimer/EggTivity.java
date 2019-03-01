d6da4616ca4fe76aa00526a21daead09ab852108
Marc Rohwedder Kær <zbcmakar16@zbc.dk>
2019-03-01 20:07:17 +0100

    Refactored code to use an handler instead of CountDown

--- app/src/main/java/dk/marc/eggtimer/EggTivity.java
+++ app/src/main/java/dk/marc/eggtimer/EggTivity.java
@@ -1,6 +1,7 @@
 package dk.marc.eggtimer;
 
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,16 +13,16 @@ import java.util.Locale;
 @SuppressWarnings("SpellCheckingInspection")
 public class EggTivity extends AppCompatActivity {
 
-    private static final long START_TIME_SOFT_BOILED_IN_MILLIS = 3000;
-    private static final long START_TIME_MEDIUM_BOILED_IN_MILLIS = 420000;
-    private static final long START_TIME_HARD_BOILED_IN_MILLIS = 600000;
+    private static final long START_TIME_SOFT_BOILED_IN_MILLIS = 300000; // 5 mins
+    private static final long START_TIME_MEDIUM_BOILED_IN_MILLIS = 420000; // 7 mins
+    private static final long START_TIME_HARD_BOILED_IN_MILLIS = 600000; // 10 mins
 
     private long mCurrentTimeSelected;
     private Button mButtonStartStop;
     private TextView mTextViewCountDown;
     private CountDownTimer mCountDownTimer;
-    private boolean mTimerRunning;
-    private long mTimeLeftInMillis;
+    private boolean isRunning;
+    private long timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +36,8 @@ public class EggTivity extends AppCompatActivity {
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
 
-        outState.putLong("millisLeft", mTimeLeftInMillis);
-        outState.putBoolean("timerRunning", mTimerRunning);
+        outState.putLong("millisLeft", timer);
+        outState.putBoolean("timerRunning", isRunning);
     }
 
     @Override
@@ -44,24 +45,25 @@ public class EggTivity extends AppCompatActivity {
         super.onRestoreInstanceState(savedInstanceState);
 
         // Get the values needed from saved state
-        mTimerRunning = savedInstanceState.getBoolean("timerRunning");
-        mTimeLeftInMillis = savedInstanceState.getLong("millisLeft");
+        isRunning = savedInstanceState.getBoolean("timerRunning");
+        timer = savedInstanceState.getLong("millisLeft");
         // We need to set the text again
         updateCountDownText();
 
         // if the timer is running we need to start it again from where it left off
-        if (mTimerRunning) {
-            startTimer(mTimeLeftInMillis);
+        if (isRunning) {
+            startTimer(timer);
             mButtonStartStop.setEnabled(true);
         }
     }
+
     /**
      * Sets the timer text and enables the button for starting the timer.
      *
-     * @param view
+     * @param view View
      */
     public void onButtonEggSelectedClicked(View view) {
-        if (!mTimerRunning) {
+        if (!isRunning) {
             switch (view.getId()) {
                 case R.id.button_soft:
                     // Enable start_button
@@ -93,8 +95,8 @@ public class EggTivity extends AppCompatActivity {
      * Used for updating the text in the view.
      */
     private void updateCountDownText() {
-        int minutes = (int) (mTimeLeftInMillis / 1000) / 60;
-        int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
+        int minutes = (int) (timer / 1000) / 60;
+        int seconds = (int) (timer / 1000) % 60;
         String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
 
         mTextViewCountDown.setText(timeLeftFormatted);
@@ -113,43 +115,53 @@ public class EggTivity extends AppCompatActivity {
      * @param view
      */
     public void OnButtonStartStopClicked(View view) {
-        if (!mTimerRunning)
+        if (!isRunning)
             startTimer(mCurrentTimeSelected);
         else
             stopTimer();
     }
+
     /**
      * Starts a timer counting down from given milliseconds
      *
      * @param timeInMillis <i>long</i> milliseconds
      */
     private void startTimer(long timeInMillis) {
-        mCountDownTimer = new CountDownTimer(timeInMillis, 1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                mTimeLeftInMillis = millisUntilFinished;
-                updateCountDownText();
-            }
 
+        timer = timeInMillis;
+        // TODO: Update buttontext
+        mButtonStartStop.setText(R.string.eggtivity_stop);
+        Handler handler = new Handler();
+        handler.post(new Runnable() {
             @Override
-            public void onFinish() {
-                mTimerRunning = !mTimerRunning;
-                mButtonStartStop.setText(R.string.eggtivity_start);
-                mButtonStartStop.setEnabled(false);
-                mTextViewCountDown.setText("00:00");
+            public void run() {
+                final Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        timer -= 1000;
+                        updateCountDownText();
+
+                        if (timer <= 0) {
+                            stopTimer();
+                        } else {
+                            handler.postDelayed(this, 1000);
+                        }
+                    }
+                }, 1000);
             }
-        }.start(); // Because we want to start this timer right now
-        mTimerRunning = !mTimerRunning;
+        });
+        isRunning = !isRunning;
         mButtonStartStop.setText(R.string.eggtivity_stop);
     }
 
     /**
-     * Stops the timer, and sets Enabled to false
+     * When this method is called, the button is renamed to "Start" and enabled set to false
+     * the boolean isRunning is set to false
      */
     private void stopTimer() {
-        mCountDownTimer.cancel();
         mTextViewCountDown.setText(R.string.eggtivity_default_time);
-        mTimerRunning = !mTimerRunning;
+        isRunning = !isRunning;
         mButtonStartStop.setEnabled(false);
     }
 
